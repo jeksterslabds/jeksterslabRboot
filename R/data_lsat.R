b9eea59a2037bcd07ead6a66b0c9b1cd6c890d74
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-07-03 05:30:56 +0800

    Edits to documentation.

--- R/data_lsat.R
+++ R/data_lsat.R
@@ -1,10 +1,13 @@
 #' Law School Data
 #'
-#' @format A matrix with 15 cases and 2 variables:
+#' @format
+#' A matrix with 15 cases and 2 variables:
 #' \describe{
 #'   \item{LSAT}{Law School Admission Test.}
 #'   \item{GPA}{Grade Point Average.}
 #' }
-#' @source Bradley Efron and Robert J. Tibshirani, 1993, "An Introduction to the Bootstrap",
-#'   Table 3.1
+#' @source
+#' Efron, B., & Tibshirani, R. J. (1993).
+#' An introduction to the bootstrap. New York, N.Y: Chapman & Hall.
+#' Table 3.1
 "lsat"
