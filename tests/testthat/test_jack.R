03a3b03166d392f45d6bc73eec6d409369a6f14b
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-07-05 01:43:07 +0800

    Added pseudo-values

--- tests/testthat/test_jack.R
+++ tests/testthat/test_jack.R
@@ -1,10 +1,10 @@
 #' ---
-#' title: "Test: jackknife"
+#' title: "Test: jack"
 #' author: "Ivan Jacob Agaloos Pesigan"
 #' date: "`r Sys.Date()`"
 #' output: rmarkdown::html_vignette
 #' vignette: >
-#'   %\VignetteIndexEntry{Test: jackknife}
+#'   %\VignetteIndexEntry{Test: jack}
 #'   %\VignetteEngine{knitr::rmarkdown}
 #'   %\VignetteEncoding{UTF-8}
 #' ---
@@ -20,7 +20,7 @@ knitr::opts_chunk$set(
 #+ setup
 library(testthat)
 library(jeksterslabRboot)
-context("Test jackknife.")
+context("Test jack.")
 #'
 #' ## Parameters
 #'
@@ -50,7 +50,7 @@ results <- jack_hat(
 #+ testthat_01
 test_that("mean", {
   expect_equivalent(
-    results["mean"],
+    results[["hat"]][["mean"]],
     22
   )
 })
@@ -58,7 +58,7 @@ test_that("mean", {
 #+ testthat_02
 test_that("bias", {
   expect_equivalent(
-    results["bias"],
+    results[["hat"]][["bias"]],
     0
   )
 })
@@ -66,7 +66,7 @@ test_that("bias", {
 #+ testthat_03
 test_that("se", {
   expect_equivalent(
-    results["se"],
+    results[["hat"]][["se"]],
     0
   )
 })
@@ -74,7 +74,103 @@ test_that("se", {
 #+ testthat_04
 test_that("thetahat_jack", {
   expect_equivalent(
-    results["thetahat_jack"],
+    results[["hat"]][["thetahat_jack"]],
     22
+  )
+})
+
+#' ## Example
+#+ example
+data <- c(
+  17.23,
+  13.93,
+  15.78,
+  14.91,
+  18.21,
+  14.28,
+  18.83,
+  13.45,
+  18.71,
+  18.81,
+  11.29,
+  13.39,
+  11.57,
+  10.94,
+  15.52,
+  15.25
+)
+ps <- c(
+  1.605,
+  1.151,
+  0.998,
+  0.942,
+  2.416,
+  1.043,
+  3.122,
+  1.362,
+  2.972,
+  3.097,
+  3.308,
+  1.393,
+  2.951,
+  3.806,
+  0.958,
+  0.937
+)
+mean_ps <- 2.00389
+ci_ll <- 1.45
+ci_ul <- 2.56
+example <- jack_hat(
+  thetahat_star = sapply(X = jack(data), FUN = function(x) log(var(x))),
+  thetahat = log(var(data))
+)
+#'
+#+ testthat_05
+test_that("mean_ps", {
+  expect_equivalent(
+    round(
+      x = mean_ps,
+      digits = 3
+    ),
+    round(
+      x = example[["hat"]][["mean_ps"]],
+      digits = 3
+    )
+  )
+})
+#'
+#+ testthat_06
+test_that("ps", {
+  expect_equivalent(
+    round(
+      x = ps,
+      digits = 3
+    ),
+    round(
+      x = example[["ps"]],
+      digits = 3
+    )
+  )
+})
+#'
+#+ testthat_07
+test_that("ll", {
+  expect_equivalent(
+    ci_ll,
+    round(
+      x = example[["ci"]][["ci_2.5"]],
+      digits = 2
+    )
+  )
+})
+#'
+#+ testthat_08
+test_that("ul", {
+  expect_equivalent(
+    ci_ul,
+    round(
+      x = example[["ci"]][["ci_97.5"]],
+      digits = 2
+    )
   )
 })
