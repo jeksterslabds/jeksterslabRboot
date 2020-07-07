c79e98817b7f98c99e35bd4fe8d9b94bbe434750
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-07-07 13:50:20 +0800

    [skip ci] Automated clean.

--- tests/testthat/test_nhstplot.R
+++ tests/testthat/test_nhstplot.R
@@ -135,3 +135,16 @@ nhstplot(
   dist = "chisq",
   df = 3
 )
+
+#+ testthat_01
+test_that("error", {
+  expect_error(
+    nhstplot(
+      alpha = c(
+        0.001,
+        0.01,
+        0.05
+      )
+    )
+  )
+})
